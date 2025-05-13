ddece202879bbdbf918658053c6899dfc8761324
Karinny16 <167472526+Karinny16@users.noreply.github.com>
2025-05-13 20:23:28 -0300

    quarta aula
    Mongodb

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const http = require('http');
 
-const SocketIO = require('socket.io') ;
+const SocketIO = require('socket.io');
 
 const app = express();
 
 const server = http.createServer(app);
 const io = SocketIO(server);
+const mongoose = require('mongoose');
 
 const ejs = require('ejs');
 
@@ -18,36 +19,65 @@ app.use(express.static(path.join(__dirname, 'public')));
 // console.log(path.join(__dirname, 'public'));
 
 
-app.set('view',path.join(__dirname, 'public'));
+app.set('view', path.join(__dirname, 'public'));
 
-app.engine('html',ejs.renderFile);
+app.engine('html', ejs.renderFile);
 
-app.use('/', (req,res)=>{
+app.use('/', (req, res) => {
     res.render('index.html');
 })
+function connectDB() {
+    let dbURL = "mongodb+srv://karinnymedeiros:lunar16@cluster0.oqv0t.mongodb.net/"
+    mongoose.connect(dbURL);
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+    mongoose.connection.once('open', function () {
+        console.log('ATLAS MONGO DB CONECTADO COM SUCESSO!');
+    })
+
+}
+connectDB();
+
+let Message = mongoose.model('message', { usuario: String, data_hora: String, message: String });
 
 /*##### LOGICA DO SOCKET.IO - ENVIO E PROPRAGAÇÃO DE MENSAGEM #####*/
 
-    let messages =[];
+let messages = [];
+
+Message.find({}).then(docs => {
+    messages = docs
+}).catch(error => {
+    console.log(error);
+});
 
 /*##### ESTRUTURA DE CONEXÃO DO SOCKET.IO  #####*/
 
-io.on('connection', socket=>{
+io.on('connection', socket => {
 
-        //Teste de conexão:
+    //Teste de conexão:
 
     console.log('NOVO USUÁRIO CONECTADO:' + socket.id)
     // recupera e mantem (exibe) as mensagens entre o fron e o back:
-    socket.emit('previousMessage',messages);
+    socket.emit('previousMessage', messages);
 
-        // Logica de chat quando uma mensagem é enviada:
-        socket.on('sendMessage', data =>{
+    // Logica de chat quando uma mensagem é enviada:
+    socket.on('sendMessage', data => {
 
-      //Adiciona a mensagem no final do array da mensagens:
-      messages.push(data);
-      socket.broadcast.emit('receiveMessage',data);
+        //Adiciona a mensagem no final do array da mensagens:
+        //   messages.push(data);
+        let message = new Message(data)
 
-        });
+        message.save()
+            .then(
+                socket.broadcast.emit('receiveMessage', data)
+            )
+            .catch(error => {
+                console.log(error);
+
+            });
+        console.log("QTD MENSAGENS:" + messages.length);
+
+    });
+    console.log("QTD MENSAGENS:" + messages.length);
 
 })
 
